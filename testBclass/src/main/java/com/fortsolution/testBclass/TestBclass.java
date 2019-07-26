150c57c1baf68490b88c4fea5a00a32e1641ceec
Ghulam Murtaza <murtaza@fortsolution.com>
2019-07-26 23:40:56 +0500

    testBclass gradle project, in this project jar is also using in the springrestgr

---
+++ testBclass/src/main/java/com/fortsolution/testBclass/TestBclass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.fortsolution.testBclass;
+
+/**
+ * @author Ghuam Murtaza
+ *
+ * Jul 26, 2019
+ * Company fortsolution.com 
+ */
+public class TestBclass {
+	
+	public String getResult()
+	{
+		
+		return "fine here murtaza Class ******B****** Result is coming fine";
+	}
+
+}
