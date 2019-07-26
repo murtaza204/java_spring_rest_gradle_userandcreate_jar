bfe2da9d9295516b410a42b3f30d3a23a85f0522
Ghulam Murtaza <murtaza@fortsolution.com>
2019-07-26 23:38:06 +0500

    testAclass gradle project their jar is using in the springrestgr project

---
+++ testAclass/src/main/java/com/fortsolution/aclassprojecct/TestAclass.java
@@ -0,0 +1,16 @@
+package com.fortsolution.aclassprojecct;
+/**
+ * 
+ * @author Ghuam Murtaza
+ *
+ * Jul 26, 2019
+ * Company fortsolution.com
+ */
+public class TestAclass {
+	
+	public String getResult()
+	{
+		
+		return "Ghulam murtaza Class ********A******* Result is coming fine";
+	}
+}
